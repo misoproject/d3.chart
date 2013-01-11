4ba65f5970c831701685e0cc8726c4f64ffef6d0
Mike Pennisi <mike@mikepennisi.com>
2013-01-11 14:42:30 -0500

    Organize chord diagram logic by "layers"
    The diagram's ticks, chords, and handles are (mostly) independent
    components. This structure communicates that fact.

--- scripts/chorddiagram.js
+++ scripts/chorddiagram.js
@@ -17,39 +17,71 @@ window.chord = function(options) {
     .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  function onEnterHandles() {
-    this.style("fill", function(d) { return fill(d.index); })
-        .style("stroke", function(d) { return fill(d.index); })
-        .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
-        .on("mouseover", fade(.1))
-        .on("mouseout", fade(1));
-  }
+  // This object serves as a namespace for this diagram's layers
+  var layers = {};
 
-  function onEnterTicks() {
-    this.attr("transform", function(d) {
-          return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-              + "translate(" + outerRadius + ",0)";
-        });
-    this.append("line")
-        .attr("x1", 1)
-        .attr("y1", 0)
-        .attr("x2", 5)
-        .attr("y2", 0)
-        .style("stroke", "#000");
+  layers.handles = function(chord) {
 
-    this.append("text")
-        .attr("x", 8)
-        .attr("dy", ".35em")
-        .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
-        .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-        .text(function(d) { return d.label; });
-  }
+    function onEnter() {
+      this.style("fill", function(d) { return fill(d.index); })
+          .style("stroke", function(d) { return fill(d.index); })
+          .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
+          .on("mouseover", fade(.1))
+          .on("mouseout", fade(1));
+    }
 
-  function onEnterChords() {
-    this.attr("d", d3.svg.chord().radius(innerRadius))
-        .style("fill", function(d) { return fill(d.target.index); })
-        .style("opacity", 1);
-  }
+    var handles = svg.append("g").selectAll("path")
+        .data(chord.groups);
+    var enteringHandles = handles.enter().append("path");
+
+    enteringHandles.call(onEnter);
+  };
+
+  layers.ticks = function(chord) {
+
+    function onEnter() {
+      this.attr("transform", function(d) {
+            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+                + "translate(" + outerRadius + ",0)";
+          });
+      this.append("line")
+          .attr("x1", 1)
+          .attr("y1", 0)
+          .attr("x2", 5)
+          .attr("y2", 0)
+          .style("stroke", "#000");
+
+      this.append("text")
+          .attr("x", 8)
+          .attr("dy", ".35em")
+          .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
+          .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+          .text(function(d) { return d.label; });
+    }
+
+    var ticks = svg.append("g").selectAll("g")
+        .data(chord.groups)
+      .enter().append("g").selectAll("g")
+        .data(groupTicks);
+    var enteringTicks = ticks.enter().append("g");
+
+    enteringTicks.call(onEnter);
+  };
+
+  layers.chords = function(chord) {
+    function onEnterChords() {
+      this.attr("d", d3.svg.chord().radius(innerRadius))
+          .style("fill", function(d) { return fill(d.target.index); })
+          .style("opacity", 1);
+    }
+    var chords = svg.append("g")
+        .attr("class", "chord")
+      .selectAll("path")
+        .data(chord.chords);
+    var enteringChords = chords.enter().append("path");
+
+    enteringChords.call(onEnterChords);
+  };
 
   function chord(matrix) {
 
@@ -58,27 +90,9 @@ window.chord = function(options) {
         .sortSubgroups(d3.descending)
         .matrix(matrix);
 
-    var handles = svg.append("g").selectAll("path")
-        .data(chord.groups);
-    var enteringHandles = handles.enter().append("path");
-
-    enteringHandles.call(onEnterHandles);
-
-    var ticks = svg.append("g").selectAll("g")
-        .data(chord.groups)
-      .enter().append("g").selectAll("g")
-        .data(groupTicks);
-    var enteringTicks = ticks.enter().append("g");
-
-    enteringTicks.call(onEnterTicks);
-
-    var chords = svg.append("g")
-        .attr("class", "chord")
-      .selectAll("path")
-        .data(chord.chords);
-    var enteringChords = chords.enter().append("path");
-
-    enteringChords.call(onEnterChords);
+    layers.handles(chord);
+    layers.ticks(chord);
+    layers.chords(chord);
   };
 
   // Returns an array of tick angles and labels, given a group.
