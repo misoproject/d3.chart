799b3fe029887a3b94654d29fec870769e06c1a8
Mike Pennisi <mike@mikepennisi.com>
2013-01-11 14:09:29 -0500

    Refactor chord diagram
    Structure according to [Towards Reusable
    Charts](http://bost.ocks.org/mike/chart/)

--- scripts/app.js
+++ scripts/app.js
@@ -9,4 +9,15 @@
     myBarChart();
   }, 1500);
 
+  var myChord = chord();
+  // From http://mkweb.bcgsc.ca/circos/guide/tables/
+  var matrix = [
+    [11975,  5871, 8916, 2868],
+    [ 1951, 10048, 2060, 6171],
+    [ 8010, 16145, 8090, 8045],
+    [ 1013,   990,  940, 6907]
+  ];
+
+  myChord(matrix);
+
 }(this));
--- scripts/chorddiagram.js
+++ scripts/chorddiagram.js
@@ -1,20 +1,9 @@
-(function(window, undefined) {
+window.chord = function(options) {
 
-  // From http://mkweb.bcgsc.ca/circos/guide/tables/
-  var matrix = [
-    [11975,  5871, 8916, 2868],
-    [ 1951, 10048, 2060, 6171],
-    [ 8010, 16145, 8090, 8045],
-    [ 1013,   990,  940, 6907]
-  ];
+  options = options || {};
 
-  var chord = d3.layout.chord()
-      .padding(.05)
-      .sortSubgroups(d3.descending)
-      .matrix(matrix);
-
-  var width = 960,
-      height = 500,
+  var width = options.width || 960,
+      height = options.height || 500,
       innerRadius = Math.min(width, height) * .41,
       outerRadius = innerRadius * 1.1;
 
@@ -28,47 +17,55 @@
     .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  svg.append("g").selectAll("path")
-      .data(chord.groups)
-    .enter().append("path")
-      .style("fill", function(d) { return fill(d.index); })
-      .style("stroke", function(d) { return fill(d.index); })
-      .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
-      .on("mouseover", fade(.1))
-      .on("mouseout", fade(1));
+  function chord(matrix) {
 
-  var ticks = svg.append("g").selectAll("g")
-      .data(chord.groups)
-    .enter().append("g").selectAll("g")
-      .data(groupTicks)
-    .enter().append("g")
-      .attr("transform", function(d) {
-        return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-            + "translate(" + outerRadius + ",0)";
-      });
+    var chord = d3.layout.chord()
+        .padding(.05)
+        .sortSubgroups(d3.descending)
+        .matrix(matrix);
 
-  ticks.append("line")
-      .attr("x1", 1)
-      .attr("y1", 0)
-      .attr("x2", 5)
-      .attr("y2", 0)
-      .style("stroke", "#000");
+    svg.append("g").selectAll("path")
+        .data(chord.groups)
+      .enter().append("path")
+        .style("fill", function(d) { return fill(d.index); })
+        .style("stroke", function(d) { return fill(d.index); })
+        .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
+        .on("mouseover", fade(.1))
+        .on("mouseout", fade(1));
 
-  ticks.append("text")
-      .attr("x", 8)
-      .attr("dy", ".35em")
-      .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
-      .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-      .text(function(d) { return d.label; });
+    var ticks = svg.append("g").selectAll("g")
+        .data(chord.groups)
+      .enter().append("g").selectAll("g")
+        .data(groupTicks)
+      .enter().append("g")
+        .attr("transform", function(d) {
+          return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+              + "translate(" + outerRadius + ",0)";
+        });
 
-  svg.append("g")
-      .attr("class", "chord")
-    .selectAll("path")
-      .data(chord.chords)
-    .enter().append("path")
-      .attr("d", d3.svg.chord().radius(innerRadius))
-      .style("fill", function(d) { return fill(d.target.index); })
-      .style("opacity", 1);
+    ticks.append("line")
+        .attr("x1", 1)
+        .attr("y1", 0)
+        .attr("x2", 5)
+        .attr("y2", 0)
+        .style("stroke", "#000");
+
+    ticks.append("text")
+        .attr("x", 8)
+        .attr("dy", ".35em")
+        .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
+        .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+        .text(function(d) { return d.label; });
+
+    svg.append("g")
+        .attr("class", "chord")
+      .selectAll("path")
+        .data(chord.chords)
+      .enter().append("path")
+        .attr("d", d3.svg.chord().radius(innerRadius))
+        .style("fill", function(d) { return fill(d.target.index); })
+        .style("opacity", 1);
+  };
 
   // Returns an array of tick angles and labels, given a group.
   function groupTicks(d) {
@@ -91,4 +88,6 @@
     };
   }
 
-}(this));
+  return chord;
+
+};
