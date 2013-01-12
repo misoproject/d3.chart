eabd929bc276cb91edf9b942757782b609f94c8b
Mike Pennisi <mike@mikepennisi.com>
2013-01-12 18:29:54 -0500

    Re-factor chord diagram to use d3.layer
    Implement minimal changes to take advantage of the newly-introduced
    d3.Layer framework. Forthcoming refactoring will improve readability
    and efficiency.

--- scripts/chorddiagram.js
+++ scripts/chorddiagram.js
@@ -17,83 +17,85 @@ window.chord = function(options) {
     .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  // This object serves as a namespace for this diagram's layers
-  var layers = {};
-
-  layers.handles = function(chord) {
-
-    function onEnter() {
-      this.style("fill", function(d) { return fill(d.index); })
-          .style("stroke", function(d) { return fill(d.index); })
-          .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
-          .on("mouseover", fade(.1))
-          .on("mouseout", fade(1));
-    }
-
-    var handles = svg.append("g").selectAll("path")
-        .data(chord.groups);
-    var enteringHandles = handles.enter().append("path");
-
-    enteringHandles.call(onEnter);
-  };
-
-  layers.ticks = function(chord) {
-
-    function onEnter() {
-      this.attr("transform", function(d) {
-            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-                + "translate(" + outerRadius + ",0)";
-          });
-      this.append("line")
-          .attr("x1", 1)
-          .attr("y1", 0)
-          .attr("x2", 5)
-          .attr("y2", 0)
-          .style("stroke", "#000");
-
-      this.append("text")
-          .attr("x", 8)
-          .attr("dy", ".35em")
-          .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
-          .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-          .text(function(d) { return d.label; });
-    }
-
-    var ticks = svg.append("g").selectAll("g")
-        .data(chord.groups)
-      .enter().append("g").selectAll("g")
-        .data(groupTicks);
-    var enteringTicks = ticks.enter().append("g");
-
-    enteringTicks.call(onEnter);
-  };
-
-  layers.chords = function(chord) {
-    function onEnterChords() {
-      this.attr("d", d3.svg.chord().radius(innerRadius))
-          .style("fill", function(d) { return fill(d.target.index); })
-          .style("opacity", 1);
-    }
-    var chords = svg.append("g")
-        .attr("class", "chord")
-      .selectAll("path")
-        .data(chord.chords);
-    var enteringChords = chords.enter().append("path");
-
-    enteringChords.call(onEnterChords);
-  };
-
   function chord(matrix) {
 
-    var chord = d3.layout.chord()
+    var chordLayout = d3.layout.chord()
         .padding(.05)
         .sortSubgroups(d3.descending)
         .matrix(matrix);
 
-    layers.handles(chord);
-    layers.ticks(chord);
-    layers.chords(chord);
+    layers.handles.draw(chordLayout);
+    layers.ticks.draw(chordLayout);
+    layers.chords.draw(chordLayout);
   };
+
+  // This object serves as a namespace for this diagram's layers.
+  var layers = {};
+
+  // Expose the layers on the draw function to promote condigurability.
+  chord.layers = layers;
+
+  layers.handles = svg.append("g").layer({
+    dataBind: function(chord) {
+      return this.selectAll("path").data(chord.groups);
+    },
+    insert: function() {
+      return this.append("path");
+    }
+  });
+  layers.handles.on("enter", function() {
+    this.style("fill", function(d) { return fill(d.index); })
+        .style("stroke", function(d) { return fill(d.index); })
+        .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
+        .on("mouseover", fade(.1))
+        .on("mouseout", fade(1));
+  });
+
+  layers.ticks = svg.append("g").layer({
+    dataBind: function(chord) {
+      return svg.append("g").selectAll("g")
+          .data(chord.groups)
+        .enter().append("g").selectAll("g")
+          .data(groupTicks);
+    },
+    insert: function() {
+      return this.append("g");
+    }
+  });
+  layers.ticks.on("enter", function() {
+    this.attr("transform", function(d) {
+          return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+              + "translate(" + outerRadius + ",0)";
+        });
+    this.append("line")
+        .attr("x1", 1)
+        .attr("y1", 0)
+        .attr("x2", 5)
+        .attr("y2", 0)
+        .style("stroke", "#000");
+
+    this.append("text")
+        .attr("x", 8)
+        .attr("dy", ".35em")
+        .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
+        .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+        .text(function(d) { return d.label; });
+  });
+
+  layers.chords = svg.append("g").attr("class", "chord").layer({
+    dataBind: function(chord) {
+      return this.selectAll("path").data(chord.chords);
+    },
+    insert: function() {
+      return this.append("path");
+    }
+  });
+  layers.chords.on("enter", function() {
+    this.attr("d", d3.svg.chord().radius(innerRadius))
+        .style("fill", function(d) { return fill(d.target.index); })
+        .style("opacity", 1);
+  });
+
 
   // Returns an array of tick angles and labels, given a group.
   function groupTicks(d) {
