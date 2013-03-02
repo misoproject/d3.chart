07e0a6c969706e4f5931aad16782e9839398dbba
Mike Pennisi <mike@mikepennisi.com>
2013-03-02 18:17:01 -0500

    Refactor Chord Diagram
    Structure according to [Towards Reusable
    Charts](http://bost.ocks.org/mike/chart/)
    
    White space is a little wonky in order to simplify this commit's diff.
    The next commit will address this.

--- examples/scripts/app.js
+++ examples/scripts/app.js
@@ -5,6 +5,14 @@
   var d3 = window.d3;
   var DataSrc = window.DataSrc;
   var BarChart = window.BarChart;
+  var Chord = window.Chord;
+  // From http://mkweb.bcgsc.ca/circos/guide/tables/
+  var matrix = [
+    [11975,  5871, 8916, 2868],
+    [ 1951, 10048, 2060, 6171],
+    [ 8010, 16145, 8090, 8045],
+    [ 1013,   990,  940, 6907]
+  ];
 
   var dataSrc = new DataSrc();
   var myBarChart = BarChart({ data: dataSrc.data });
@@ -13,5 +21,8 @@
     dataSrc.fetch();
     myBarChart();
   }, 1500);
+
+  var myChord = Chord();
+  myChord(matrix);
 
 }(this));
