3743027fa38c3146635e063b0084f39ecc5c2621
Mike Pennisi <mike@mikepennisi.com>
2013-03-02 21:31:06 -0500

    Refactor Bar Chart to use d3.chart
    The white space is a little wonky. It will be fixed in a later commit in
    order to improve the legibility of this commit's diff.

--- examples/scripts/bar-chart.js
+++ examples/scripts/bar-chart.js
@@ -1,87 +1,86 @@
-window.BarChart = function(options) {
+d3.chart("BarChart", {
 
-  "use strict";
+  initialize: function(options) {
 
   options = options || {};
 
-  var w, h,
-    data = options.data || [];
+  var chart = this;
+  this.data = options.data || [];
 
-  var x = d3.scale.linear()
-    .domain([0, data.length]);
+  this.x = d3.scale.linear()
+    .domain([0, this.data.length]);
 
-  var y = d3.scale.linear()
+  this.y = d3.scale.linear()
     .domain([0, 100]);
 
-  var svg = d3.select("body").append("svg")
+  this.base
     .attr("class", "chart");
 
   function onEnter() {
-    this.attr("x", function(d, i) { return x(i + 1) - .5; })
-        .attr("y", function(d) { return h - y(d.value) - .5; })
-        .attr("width", w / data.length)
-        .attr("height", function(d) { return y(d.value); });
+    this.attr("x", function(d, i) { return chart.x(i + 1) - .5; })
+        .attr("y", function(d) { return chart.h - chart.y(d.value) - .5; })
+        .attr("width", chart.w / chart.data.length)
+        .attr("height", function(d) { return chart.y(d.value); });
   }
 
   function onEnterTrans() {
     this.duration(1000)
-        .attr("x", function(d, i) { return x(i) - .5; });
+        .attr("x", function(d, i) { return chart.x(i) - .5; });
   }
 
   function onTrans() {
     this.duration(1000)
-        .attr("x", function(d, i) { return x(i) - .5; });
+        .attr("x", function(d, i) { return chart.x(i) - .5; });
   }
 
   function onExitTrans() {
     this.duration(1000)
-        .attr("x", function(d, i) { return x(i - 1) - .5; })
+        .attr("x", function(d, i) { return chart.x(i - 1) - .5; })
         .remove();
   }
 
   function dataBind() {
 
     return this.selectAll("rect")
-      .data(data, function(d) { return d.time; });
+      .data(chart.data, function(d) { return d.time; });
   }
 
   function insert() {
     return this.insert("rect", "line");
   }
 
-  var chart = svg.layer({
+  this.layers.bars = this.base.layer({
     dataBind: dataBind,
     insert: insert
   });
 
-  chart.on("enter", onEnter);
-  chart.on("enter:transition", onEnterTrans);
-  chart.on("update:transition", onTrans);
-  chart.on("exit:transition", onExitTrans);
+  this.layers.bars.on("enter", onEnter);
+  this.layers.bars.on("enter:transition", onEnterTrans);
+  this.layers.bars.on("update:transition", onTrans);
+  this.layers.bars.on("exit:transition", onExitTrans);
+  this.width(options.width || 600);
+  this.height(options.height || 80);
 
-  chart.width = function(newWidth) {
+  },
+
+  width: function(newWidth) {
     if (!arguments.length) {
-      return w;
+      return this.w;
     }
-    w = newWidth;
-    x.range([0, w]);
-    svg.attr("width", w);
+    this.w = newWidth;
+    this.x.range([0, this.w]);
+    this.base.attr("width", this.w);
     return this;
-  };
+  },
 
-  chart.height = function(newHeight) {
+  height: function(newHeight) {
     if (!arguments.length) {
-      return h;
+      return this.h;
     }
-    h = newHeight;
-    y.rangeRound([0, h]);
-    svg.attr("height", h);
+    this.h = newHeight;
+    this.y.rangeRound([0, this.h]);
+    this.base.attr("height", this.h);
     return this;
-  };
+  }
 
-  chart.width(options.width || 600);
-  chart.height(options.height || 80);
-
-  return chart;
-
-};
+});
