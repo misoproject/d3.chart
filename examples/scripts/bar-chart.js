90151a2ef56efa17b38b269d47b1e37b865d6750
Mike Pennisi <mike@mikepennisi.com>
2013-03-02 22:00:31 -0500

    Fix white space

--- examples/scripts/bar-chart.js
+++ examples/scripts/bar-chart.js
@@ -2,64 +2,64 @@ d3.chart("BarChart", {
 
   initialize: function(options) {
 
-  options = options || {};
+    options = options || {};
 
-  var chart = this;
-  this.data = options.data || [];
+    var chart = this;
+    this.data = options.data || [];
 
-  this.x = d3.scale.linear()
-    .domain([0, this.data.length]);
+    this.x = d3.scale.linear()
+      .domain([0, this.data.length]);
 
-  this.y = d3.scale.linear()
-    .domain([0, 100]);
+    this.y = d3.scale.linear()
+      .domain([0, 100]);
 
-  this.base
-    .attr("class", "chart");
+    this.base
+      .attr("class", "chart");
 
-  function onEnter() {
-    this.attr("x", function(d, i) { return chart.x(i + 1) - .5; })
-        .attr("y", function(d) { return chart.h - chart.y(d.value) - .5; })
-        .attr("width", chart.w / chart.data.length)
-        .attr("height", function(d) { return chart.y(d.value); });
-  }
+    function onEnter() {
+      this.attr("x", function(d, i) { return chart.x(i + 1) - .5; })
+          .attr("y", function(d) { return chart.h - chart.y(d.value) - .5; })
+          .attr("width", chart.w / chart.data.length)
+          .attr("height", function(d) { return chart.y(d.value); });
+    }
 
-  function onEnterTrans() {
-    this.duration(1000)
-        .attr("x", function(d, i) { return chart.x(i) - .5; });
-  }
+    function onEnterTrans() {
+      this.duration(1000)
+          .attr("x", function(d, i) { return chart.x(i) - .5; });
+    }
 
-  function onTrans() {
-    this.duration(1000)
-        .attr("x", function(d, i) { return chart.x(i) - .5; });
-  }
+    function onTrans() {
+      this.duration(1000)
+          .attr("x", function(d, i) { return chart.x(i) - .5; });
+    }
 
-  function onExitTrans() {
-    this.duration(1000)
-        .attr("x", function(d, i) { return chart.x(i - 1) - .5; })
-        .remove();
-  }
+    function onExitTrans() {
+      this.duration(1000)
+          .attr("x", function(d, i) { return chart.x(i - 1) - .5; })
+          .remove();
+    }
 
-  function dataBind() {
+    function dataBind() {
 
-    return this.selectAll("rect")
-      .data(chart.data, function(d) { return d.time; });
-  }
+      return this.selectAll("rect")
+        .data(chart.data, function(d) { return d.time; });
+    }
 
-  function insert() {
-    return this.insert("rect", "line");
-  }
+    function insert() {
+      return this.insert("rect", "line");
+    }
 
-  this.layers.bars = this.base.layer({
-    dataBind: dataBind,
-    insert: insert
-  });
+    this.layers.bars = this.base.layer({
+      dataBind: dataBind,
+      insert: insert
+    });
 
-  this.layers.bars.on("enter", onEnter);
-  this.layers.bars.on("enter:transition", onEnterTrans);
-  this.layers.bars.on("update:transition", onTrans);
-  this.layers.bars.on("exit:transition", onExitTrans);
-  this.width(options.width || 600);
-  this.height(options.height || 80);
+    this.layers.bars.on("enter", onEnter);
+    this.layers.bars.on("enter:transition", onEnterTrans);
+    this.layers.bars.on("update:transition", onTrans);
+    this.layers.bars.on("exit:transition", onExitTrans);
+    this.width(options.width || 600);
+    this.height(options.height || 80);
 
   },
 
