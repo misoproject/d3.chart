df4f430631e64153ce4d286567cb04d90ca5df01
Mike Pennisi <mike@mikepennisi.com>
2013-07-12 16:45:37 -0400

    Break API definitions into separate files

--- src/init.js
+++ src/init.js
@@ -14,3 +14,12 @@ d3Chart.assert = function(test, message) {
 d3Chart.assert(d3, "d3.js is required");
 d3Chart.assert(typeof d3.version === "string" && d3.version.match(/^3/),
 	"d3.js version 3 is required");
+
+var Surrogate = function(ctor) { this.constructor = ctor; };
+var variadicNew = function(Ctor, args) {
+	var inst;
+	Surrogate.prototype = Ctor.prototype;
+	inst = new Surrogate(Ctor);
+	Ctor.apply(inst, args);
+	return inst;
+};
