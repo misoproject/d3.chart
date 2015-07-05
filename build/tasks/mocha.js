9a051607e44c671206b7a3fec50a22f1de78c63a
Mike Pennisi <mike@mikepennisi.com>
2015-07-05 11:56:47 -0400

    Limit repetition in testing infrastructure
    Re-use the same `index.html` file for all tests. This limits the code
    that must be repeated across files and promotes consistency between test
    runs.

--- build/tasks/mocha.js
+++ build/tasks/mocha.js
@@ -1,25 +1,50 @@
 module.exports = function(grunt) {
 	"use strict";
 
+	/**
+	 * In order to limit duplication (and promote consistency), the testing
+	 * configuration re-uses the same `index.html` file for all tests. The
+	 * test setup logic implements branching behavior based on the value of the
+	 * userAgent string, which is set programatically according to the
+	 * configuration below. (This information cannot be specified using a query
+	 * string parameter because `index.html` is loaded via the file://
+	 * protocol.)
+	 */
+
 	grunt.config.set("mocha", {
+		options: {
+			run: false
+		},
 		unit: {
 			src: ["test/index.html"],
 			options: {
-				run: true
+				page: {
+					settings: {
+						userAgent: "PhantomJS:testSource(direct)"
+					}
+				}
 			}
 		},
 		// Test that the distributed source files function correctly in AMD and
 		// "browser global" environments.
 		exportsAmd: {
-			src: ["test/exports/amd.html"],
+			src: ["test/index.html"],
 			options: {
-				run: false
+				page: {
+					settings: {
+						userAgent: "PhantomJS:testSource(amd)"
+					}
+				}
 			}
 		},
 		exportsGlobal: {
-			src: ["test/exports/global.html"],
+			src: ["test/index.html"],
 			options: {
-				run: true
+				page: {
+					settings: {
+						userAgent: "PhantomJS:testSource(global)"
+					}
+				}
 			}
 		}
 	});
